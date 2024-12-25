e577a4897f85e0aea783aed04490737a32569d8a
Saeed Bazargan <s.bazargan@tosantechno.net>
2024-12-25 15:20:36 +0330

    I added a code for the ethernet and ping, but it can not get a ping from my pc and i dont know why.

---
+++ STM32H745ZI_Nucleo/STM32H745Nucleo_Ethernet_Ping_NoMemConfig/Ethernet_Ping_NoMemConfig/CM7/Debug/Drivers/BSP/Components/lan8742.d
@@ -0,0 +1,4 @@
+Drivers/BSP/Components/lan8742.o: \
+ E:/sbzrgn/My-STM32_Discoveries/STM32H745ZI_Nucleo/STM32H745Nucleo_Ethernet_Ping_NoMemConfig/Ethernet_Ping_NoMemConfig/Drivers/BSP/Components/lan8742/lan8742.c \
+ E:/sbzrgn/My-STM32_Discoveries/STM32H745ZI_Nucleo/STM32H745Nucleo_Ethernet_Ping_NoMemConfig/Ethernet_Ping_NoMemConfig/Drivers/BSP/Components/lan8742/lan8742.h
+E:/sbzrgn/My-STM32_Discoveries/STM32H745ZI_Nucleo/STM32H745Nucleo_Ethernet_Ping_NoMemConfig/Ethernet_Ping_NoMemConfig/Drivers/BSP/Components/lan8742/lan8742.h:
